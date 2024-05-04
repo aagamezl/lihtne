c7b5f08d3d99c4e054b0f778c4fd12ca3431399b
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2024-05-05 00:57:01 +0200

    feat: postgres processor unit test

--- src/Illuminate/Database/Query/Processors/PostgresProcessor.js
+++ src/Illuminate/Database/Query/Processors/PostgresProcessor.js
@@ -4,6 +4,37 @@ import Processor from './Processor.js'
 
 export default class PostgresProcessor extends Processor {
   /**
+   * Process the results of a columns query.
+   *
+   * @param {Array<Object>} results
+   * @returns {Array<Object>}
+   */
+  processColumns (results) {
+    return results.map(result => {
+      const autoincrement = result.default !== null && result.default.startsWith('nextval(')
+
+      return {
+        name: result.name,
+        type_name: result.type_name,
+        type: result.type,
+        collation: result.collation,
+        nullable: Boolean(result.nullable),
+        default: result.generated ? null : result.default,
+        auto_increment: autoincrement,
+        comment: result.comment,
+        generation: result.generated
+          ? {
+              type: {
+                s: 'stored'
+              }[result.generated] ?? null,
+              expression: result.default
+            }
+          : null
+      }
+    })
+  }
+
+  /**
    * Process an "insert get ID" query.
    *
    * @param  {import('./../Builder').default}  query
