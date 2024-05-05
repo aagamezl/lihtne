4f915a05f1aec01aefbfcc833876ca49f8e34fa5
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2024-05-05 22:08:35 +0200

    feat: sQLite processor unit test

---
+++ src/Illuminate/Database/Query/Processors/SQLiteProcessor.js
@@ -0,0 +1,101 @@
+import Processor from './Processor.js'
+
+export default class SQLiteProcessor extends Processor {
+  /**
+   * Process the results of a columns query.
+   *
+   * @param {Array} results - The results of the columns query.
+   * @param {string} sql - The SQL query.
+   * @returns {Array} - Processed column results.
+   */
+  processColumns (results, sql = '') {
+    const hasPrimaryKey = results.reduce((acc, result) => acc + Number(result.primary), 0) === 1
+
+    return results.map(result => {
+      const type = result.type.toLowerCase()
+
+      const collationMatch = sql.match(new RegExp(`\\b${result.name}\\b[^,(]+(?:\\([^()]+\\)[^,]*)?(?:(?:default|check|as)\\s*(?:\\(.*?\\))?[^,]*)*collate\\s+["'\`]?(\\w+)`, 'i'))
+      const collation = collationMatch ? collationMatch[1].toLowerCase() : null
+
+      const isGenerated = [2, 3].includes(result.extra)
+
+      const expressionMatch = isGenerated && sql.match(new RegExp(`\\b${result.name}\\b[^,]+\\s+as\\s+\\(((?:[^()]+|\\((?:[^()]+|\\([^()]*\\))*\\))*)\\)`, 'i'))
+      const expression = expressionMatch ? expressionMatch[1] : null
+
+      return {
+        name: result.name,
+        type_name: type.split('(')[0] || '',
+        type,
+        collation,
+        nullable: Boolean(result.nullable),
+        default: result.default,
+        auto_increment: hasPrimaryKey && result.primary && type === 'integer',
+        comment: null,
+        generation: isGenerated
+          ? {
+              type: (() => {
+                switch (Number(result.extra)) {
+                  case 3:
+                    return 'stored'
+                  case 2:
+                    return 'virtual'
+                  default:
+                    return null
+                }
+              })(),
+              expression
+            }
+          : null
+      }
+    })
+  }
+
+  /**
+   * Process the results of an indexes query.
+   *
+   * @param {Array} results - The results of the indexes query.
+   * @returns {Array} - Processed index results.
+   */
+  processIndexes (results) {
+    let primaryCount = 0
+
+    let indexes = results.map(result => {
+      const isPrimary = Boolean(result.primary)
+      if (isPrimary) {
+        primaryCount += 1
+      }
+
+      return {
+        name: result.name.toLowerCase(),
+        columns: result.columns.split(','),
+        type: null,
+        unique: Boolean(result.unique),
+        primary: isPrimary
+      }
+    })
+
+    if (primaryCount > 1) {
+      indexes = indexes.filter(index => index.name !== 'primary')
+    }
+
+    return indexes
+  }
+
+  /**
+   * Process the results of a foreign keys query.
+   *
+   * @param {Array} results - The results of the foreign keys query.
+   * @returns {Array} - Processed foreign key results.
+   */
+  processForeignKeys (results) {
+    return results.map(result => ({
+      name: null,
+      columns: result.columns.split(','),
+      foreign_schema: null,
+      foreign_table: result.foreign_table,
+      foreign_columns: result.foreign_columns.split(','),
+      on_update: result.on_update.toLowerCase(),
+      on_delete: result.on_delete.toLowerCase()
+    }))
+  }
+}
---
+++ test/Database/DatabaseSQLiteProcessorTest.spec.js
@@ -0,0 +1,22 @@
+import test from 'ava'
+
+import SQLiteProcessor from '../../src/Illuminate/Database/Query/Processors/SQLiteProcessor.js'
+
+test.skip('testProcessColumns', async t => {
+  const processor = new SQLiteProcessor()
+
+  const listing = [
+    { name: 'id', type_name: 'int4', type: 'integer', collation: '', nullable: true, default: "nextval('employee_id_seq'::regclass)", comment: '' },
+    { name: 'name', type_name: 'varchar', type: 'character varying(100)', collation: 'collate', nullable: false, default: '', comment: 'foo' },
+    { name: 'balance', type_name: 'numeric', type: 'numeric(8,2)', collation: '', nullable: true, default: '4', comment: 'NULL' },
+    { name: 'birth_date', type_name: 'timestamp', type: 'timestamp(6) without time zone', collation: '', nullable: false, default: '', comment: '' }
+  ]
+  const expected = [
+    { name: 'id', type_name: 'int4', type: 'integer', collation: '', nullable: true, default: "nextval('employee_id_seq'::regclass)", auto_increment: true, comment: '', generation: null },
+    { name: 'name', type_name: 'varchar', type: 'character varying(100)', collation: 'collate', nullable: false, default: '', auto_increment: false, comment: 'foo', generation: null },
+    { name: 'balance', type_name: 'numeric', type: 'numeric(8,2)', collation: '', nullable: true, default: '4', auto_increment: false, comment: 'NULL', generation: null },
+    { name: 'birth_date', type_name: 'timestamp', type: 'timestamp(6) without time zone', collation: '', nullable: false, default: '', auto_increment: false, comment: '', generation: null }
+  ]
+
+  t.deepEqual(processor.processColumns(listing), expected)
+})
